ddeb2ff669fd655f5d0802f17b10ea4d9d76159b
AbdelMadjid1506 <kessal.abdel.madjid@outlook.fr>
2021-10-29 19:57:56 +0100

    socketIO

---
+++ server.js
@@ -0,0 +1,28 @@
+const app = require('express')();
+const http= require('http').Server(app);
+const io= require('socket.io')(http);
+
+
+
+app.get('/', function(req,res){
+    res.sendFile(__dirname+ '/index.html');
+})
+
+
+
+io.on('connection', function(socket){
+    console.log("a user is connected");
+    socket.on('disconnect', function(){
+        console.log("a user is disconnect");
+    })
+ 
+    socket.on('chat message', function(msg) {
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
+        
+    });
+})
+
+http.listen('5000', function(){
+    console.log("Server running on 5000");
+})
